5f4780db3d2ea4c03482c4a3e221b31c17176396
bhagwat-chate <bhagwat.chate25@gmail.com>
2022-09-24 06:57:59 +0530

    Tuner: get_best_params_for_knn

--- best_model_finder/tuner.py
+++ best_model_finder/tuner.py
@@ -43,4 +43,28 @@ class Model_Finder:
             self.logger_object.log(self.file_object, '*** Exception occurred in get_best_params_for_random_forest method of the Model_Finder class. Exception:  ' + str(e))
             self.logger_object.log(self.file_object, 'Exited the get_best_params_for_random_forest method of the Model_Finder class')
 
+    def get_best_params_for_knn(self, train_x, train_y):
+        self.logger_object.log(self.file_object, 'Entered the get_best_params_for_knn method of the Model_Finder class')
+        try:
+            self.param_grid_knn = {
+                'algorithm': ['ball_tree', 'kd_tree', 'brute'],
+                'leaf_size': [10, 17, 24, 28, 30, 35],
+                'n_neighbours': [4, 5, 8, 10, 1],
+                'p': [1, 2]
+            }
+            self.grid = GridSearchCV(self.knn, self.param_grid_knn, verbose=3, cv=5)
+            self.grid.fit(train_x, train_y)
 
+            self.algorithm = self.grid.best_params_['algorithm']
+            self.leaf_size = self.grid.best_params_['leaf_size']
+            self.n_neighbours = self.grid.best_params_['n_neighbours']
+            self.p = self.grid.best_params_['p']
+
+            self.knn = KNeighborsClassifier(algorithm=self.algorithm, leaf_size=self.leaf_size, n_neighbors=self.n_neighbours, p=self.p, n_jobs=-1)
+            self.knn.fit(train_x, train_y)
+            self.logger_object.log(self.file_object, " KNN best parameters "+str(self.grid.best_params_))
+            self.logger_object.log(self.file_object, "Exited the get_best_params_for_knn method of the Model_Finder class")
+            return self.knn
+        except Exception as e:
+            self.logger_object.log(self.file_object,'*** Exception occurred in get_best_params_for_knn method of the Model_Finder class. Exception:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Exited the get_best_params_for_knn method of the Model_Finder class')
