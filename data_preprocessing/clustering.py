c858df421248838ae953b1bf0fb2cbef4a618470
bhagwat-chate <bhagwat.chate25@gmail.com>
2022-09-24 06:18:01 +0530

    Clustering: create clusters

--- data_preprocessing/clustering.py
+++ data_preprocessing/clustering.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 from kneed import KneeLocator
 from file_operations.file_methods import File_Operation
 
+
 class KMeansClustering:
     """
     This class shall be used to divide the data into clusters before training.
@@ -13,6 +14,7 @@ class KMeansClustering:
     def __init__(self, file_object, logger_object):
         self.file_object = file_object
         self.logger_object = logger_object
+
     def elbow_plot(self, data):
         self.logger_object.log(self.file_object, 'Entered the elbow_plot method of the KMeansClustering class')
         wcss = []
@@ -33,3 +35,19 @@ class KMeansClustering:
         except Exception as e:
             self.logger_object.log(self.file_object,'*** Exception occurred in elbow_plot method of the KMeansClustering class. Exception:  ' + str(e))
             self.logger_object.log(self.file_object,'Exited the elbow_plot method of the KMeansClustering class')
+
+    def create_clusters(self, data, number_of_clusters):
+        self.logger_object.log(self.file_object, 'Entered the create_clusters method of the KMeansClustering class')
+        self.data = data
+        try:
+            self.kmeans = KMeans(n_clusters=number_of_clusters, init="k-means++", random_state=42)
+            self.y_kmeans = self.kmeans.fit_predict(data)
+            self.file_op = File_Operation(self.file_object, self.logger_object)
+            self.save_model = self.file_op.save_model(self.kmeans, 'KMeans')
+            self.data['cluster'] = self.y_kmeans
+            self.logger_object.log(self.file_object, " clusters created, cluster count: "+str(self.kn.knee))
+            self.logger_object.log(self.file_object, "Exited the create_clusters method of the KMeansClustering class")
+            return self.data
+        except Exception as e:
+            self.logger_object.log(self.file_object, '*** Exception occurred in ******* method of the KMeansClustering class. Exception:  ' + str(e))
+            self.logger_object.log(self.file_object, 'Exited the create_clusters method of the KMeansClustering class')
